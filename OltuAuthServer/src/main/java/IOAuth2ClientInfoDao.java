2a3d885bc452651e6ea5944a048a3546b15a47bb
Garfield-Song <songbintaochina@gmail.com>
2015-12-03 17:27:10 +0800

    Create IOAuth2ClientInfoDao.java

---
+++ OltuAuthServer/src/main/java/IOAuth2ClientInfoDao.java
@@ -0,0 +1,6 @@
+package org.garfield.java.jersey.example.dao;
+
+import org.garfield.java.jersey.example.entity.OAuth2ClientInfo;
+
+public interface IOAuth2ClientInfoDao extends IBaseDao<OAuth2ClientInfo, Long>{
+}
