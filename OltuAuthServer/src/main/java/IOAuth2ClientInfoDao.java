6a6ecbfdb33c163126cd180f7da413dcced2587f
Garfield Song <songbintaochina@gmail.com>
2016-10-09 07:33:49 +0800

    remove

--- OltuAuthServer/src/main/java/IOAuth2ClientInfoDao.java
+++
@@ -1,6 +0,0 @@
-package org.garfield.java.jersey.example.dao;
-
-import org.garfield.java.jersey.example.entity.OAuth2ClientInfo;
-
-public interface IOAuth2ClientInfoDao extends IBaseDao<OAuth2ClientInfo, Long>{
-}
--- OltuAuthServer/src/main/java/IOAuth2ClientTokenDao.java
+++
@@ -1,8 +0,0 @@
-package org.garfield.java.jersey.example.dao;
-
-import org.garfield.java.jersey.example.entity.OAuth2ClientToken;
-
-public interface IOAuth2ClientTokenDao extends IBaseDao<OAuth2ClientToken, Long>{
-
-    public OAuth2ClientToken findByClientId(String clientId);
-}
